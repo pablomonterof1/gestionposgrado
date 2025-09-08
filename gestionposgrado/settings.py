aed0b3339021600becda8ad05672052690df0983
pablomonterof1 <pablo.montero.farias@utlook.com>
2025-09-08 16:41:40 -0500

    08092025 PM

--- gestionposgrado/settings.py
+++ gestionposgrado/settings.py
@@ -108,9 +108,20 @@ DATABASES = {
         'PASSWORD': 'dGZ$L~L*5qd4',
         'HOST': '66.45.232.106',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+        'CONN_MAX_AGE': 300,
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
